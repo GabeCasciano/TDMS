96a7926d9f33d57e4263c6b217c1522f00e7ca7e
GabeCasciano <gabecasciano@gmail.com>
2025-06-07 18:36:40 -0400

    made the py generate  correct tdms, will now reverse engineer it

--- test/tdms_test.py
+++ test/tdms_test.py
@@ -1,7 +1,23 @@
-from nptdms import TdmsFile
+from nptdms import TdmsFile, TdmsWriter, RootObject, GroupObject, ChannelObject
+import numpy as np
+
+root_obj = RootObject(properties={"RootTestProp": "RootTestProptValue"})
+
+group_obj = GroupObject(
+    "TestGroup", properties={"TestGroupProperties": "TestGroupPropertiesValue"}
+)
+
+data = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+chan_obj = ChannelObject("TestGroup", "TestChannel", data, properties={})
+
+with TdmsWriter("test_npTDMS.tdms") as tdms_writer:
+    tdms_writer.write_segment([root_obj, group_obj, chan_obj])
+
+    tdms_writer.write_segment([chan_obj])
 
 # Open the TDMS file
-tdms_file = TdmsFile.read("tdms_test.tdms")
+
+tdms_file = TdmsFile.read("test_npTDMS.tdms")
 
 # List all groups
 for group in tdms_file.groups():
